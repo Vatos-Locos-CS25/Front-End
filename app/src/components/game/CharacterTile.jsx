0bc74db9f1c5b3b1bd99ecfa780e32499ab00c62
Frank Martinez <frankie.martinez@gmail.com>
2020-02-05 18:13:51 -0800

    ✨ added the character back to render

--- app/src/components/game/CharacterTile.jsx
+++ app/src/components/game/CharacterTile.jsx
@@ -16,23 +16,23 @@ const SurroundingMapTile = props => {
         display: `${charTranslate.display ? "flex" : "none"}`
     }
     
-    console.log('possDirect , lastTile, direction, last move, move history --> \n', possDirect, lastTile.room_id, direction, moveHistory[moveHistory.length - 1], moveHistory);
+    // console.log('possDirect , lastTile, direction, last move, move history --> \n', possDirect, lastTile.room_id, direction, moveHistory[moveHistory.length - 1], moveHistory);
     useEffect(() => {
         if (direction === "east" && moveHistory[moveHistory.length - 1] === possDirect) console.log("this is hitting")
         if (direction === "west" && moveHistory[moveHistory.length - 1] === possDirect) console.log("this is hitting west")
-        
-        // // Move backwards
-        // if (direction === "north" && moveHistory[moveHistory.length - 2] === possDirect) setCharTranslate({ ...charTranslate, y: charTranslate.y + 50, display: true })
-        // if (direction === "south" && moveHistory[moveHistory.length - 2] === possDirect) setCharTranslate({ ...charTranslate, y: charTranslate.y - 50, display: true })
-        // if (direction === "east" && moveHistory[moveHistory.length - 2] === possDirect) setCharTranslate({ ...charTranslate, x: charTranslate.x - 50, display: true })
-        // if (direction === "west" && moveHistory[moveHistory.length - 2] === possDirect) setCharTranslate({ ...charTranslate, x: charTranslate.x + 50, display: true })
 
-        // // Move forwards
-        // if (direction === "north" && possDirect !== 0) setCharTranslate({ ...charTranslate, y: charTranslate.y - 50, display: true })
-        // else if (direction === "south" && possDirect !== 0) setCharTranslate({ ...charTranslate, y: charTranslate.y + 50, display: true })
-        // else if (direction === "east" && possDirect !== 0) setCharTranslate({ ...charTranslate, x: charTranslate.x + 50, display: true })
-        // else if (direction === "west" && possDirect !== 0) setCharTranslate({ ...charTranslate, x: charTranslate.x - 50, display: true })
-        // else setCharTranslate({ ...charTranslate, display: false })
+        // Move backwards
+        if (direction === "north" && moveHistory[moveHistory.length - 2] === possDirect) setCharTranslate({ ...charTranslate, y: charTranslate.y + 50, display: true })
+        if (direction === "south" && moveHistory[moveHistory.length - 2] === possDirect) setCharTranslate({ ...charTranslate, y: charTranslate.y - 50, display: true })
+        if (direction === "east" && moveHistory[moveHistory.length - 2] === possDirect) setCharTranslate({ ...charTranslate, x: charTranslate.x - 50, display: true })
+        if (direction === "west" && moveHistory[moveHistory.length - 2] === possDirect) setCharTranslate({ ...charTranslate, x: charTranslate.x + 50, display: true })
+
+        // Move forwards
+        if (direction === "north" && possDirect !== 0) setCharTranslate({ ...charTranslate, y: charTranslate.y - 50, display: true })
+        else if (direction === "south" && possDirect !== 0) setCharTranslate({ ...charTranslate, y: charTranslate.y + 50, display: true })
+        else if (direction === "east" && possDirect !== 0) setCharTranslate({ ...charTranslate, x: charTranslate.x + 50, display: true })
+        else if (direction === "west" && possDirect !== 0) setCharTranslate({ ...charTranslate, x: charTranslate.x - 50, display: true })
+        else setCharTranslate({ ...charTranslate, display: false })
 
         if (lastTile.room_id !== undefined) setMoveHistory(moveHistory.concat(lastTile.room_id))
     }, [possDirect])
